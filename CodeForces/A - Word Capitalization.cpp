5a2d2229f155ce4f2ad95873ad5778ab7ca5091b
GabProg <151977535+GabrielPymentel@users.noreply.github.com>
2024-12-05 11:47:42 -0400

    Create A - Word Capitalization.cpp

---
+++ CodeForces/A - Word Capitalization.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define b begin()
+#define e end()
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    string  a;
+    cin>>a;
+    a[0] = toupper(a[0]);
+    cout<<a<<endl;
+    return 0;
+}
