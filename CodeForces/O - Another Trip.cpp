8e934936ff3450189b40e8eb158ad9634ab96d79
GabProg <151977535+GabrielPymentel@users.noreply.github.com>
2024-12-05 11:58:53 -0400

    Create O - Another Trip.cpp

---
+++ CodeForces/O - Another Trip.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define ll long long 
+#define b begin()
+#define e end()
+#define vetInt vector<int>
+#define vetStr vector<string>
+#define vetChar vector<char>
+ 
+//Thank'you Willian Lin and Diego Pedro.
+//CodeForces
+//Beecrowd
+ 
+const int MxN = 1e4;
+ 
+using namespace std;
+ 
+void solve() {
+	int n,m;cin>>n>>m;
+	cout<<(n*2)+m<<endl;
+}
+ 
+int main(){
+		
+	int t = 1;
+	//cin>>t;	
+	while(t--)
+		solve();	
+	return 0;
+ 
+}
