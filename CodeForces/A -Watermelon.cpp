f40a6ea5280ebacd505906a59ef66939457568f6
GabProg <151977535+GabrielPymentel@users.noreply.github.com>
2024-12-05 11:43:41 -0400

    Create A -Watermelon.cpp

---
+++ CodeForces/A -Watermelon.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define b begin()
+#define e end()
+//Thank'you Willian Lin :D
+using namespace std;
+ 
+ 
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	
+	int w; cin>>w;
+	cout<<(w%2 == 0 &&w>2?"YES\n":"NO\n");	
+	return 0;
+}
